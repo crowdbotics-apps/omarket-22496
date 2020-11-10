a0c93a68ca18effdd1744c6c6200d23011465ae8
Crowdbotics <team@crowdbotics.com>
2020-11-10 22:34:44 +0000

    Update models

--- backend/omarket_22496/wsgi.py
+++ backend/omarket_22496/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omarket_22496.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omarket_22496.settings")
 
 application = get_wsgi_application()
